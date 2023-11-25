6ba4b66e8a21949ad2f480bfc4b02e46644e0d76
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-11-25 07:00:45 -0800

    fixed saving objects to database

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,31 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy import String, DateTime, Column
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DATETIME
 from models import storage_type
 
 Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column(String(60), nullable=True, primary_key=True, unique=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    """A base class for all hbnb models
+
+    Attributes:
+        id (sqlalchemy String): The BaseModel id.
+        created_at (sqlalchemy DateTime): The datetime at creation.
+        updated_at (sqlalchemy DateTime): The datetime of last update.
+    """
+    id = Column(String(60),
+                nullable=False,
+                primary_key=True,
+                unique=True)
+    created_at = Column(DATETIME,
+                        nullable=False,
+                        default=datetime.utcnow())
+    updated_at = Column(DATETIME,
+                        nullable=False,
+                        default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
@@ -37,8 +50,8 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -49,16 +62,16 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        my_dict = self.__dict__.copy()
-        my_dict['__class__'] = self.__class__.__name__
-        for k in my_dict:
-            if type(my_dict[k]) is datetime:
-                my_dict[k] = my_dict[k].isoformat()
-        if '_sa_instance_state' in my_dict.keys():
-            del(my_dict['_sa_instance_state'])
-        return my_dict
+        dct = self.__dict__.copy()
+        dct['__class__'] = self.__class__.__name__
+        for k in dct:
+            if type(dct[k]) is datetime:
+                dct[k] = dct[k].isoformat()
+        if '_sa_instance_state' in dct.keys():
+            del(dct['_sa_instance_state'])
+        return dct
 
     def delete(self):
-        """delete the current instance from the storage (models.storage)"""
+        '''deletes the current instance from the storage'''
         from models import storage
         storage.delete(self)
