c40e21207988fef37f91b100af5c582300b7e650
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-12-12 23:30:28 -0800

    fixed my ip addresses

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["100.25.170.230", "34.232.65.144"]
 
 
 def do_pack():
