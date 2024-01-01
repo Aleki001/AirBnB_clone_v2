335742d9c636f4f8b875340bbfb06c85ad79418e
Alex Kinyua <alexkinyua001alx@gmail.com>
2024-01-01 12:52:15 -0800

    changed code on 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,13 +4,14 @@ displays list of cities by states
 """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def task8():
     """displays states on a html template"""
-    states = storage.all("State").values()
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
