dd787275fc2589a9b577bd069e0791cc45900084
Alex Kinyua <alexkinyua001alx@gmail.com>
2024-01-02 00:27:31 -0800

    fix pycodestyle errors

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,17 +8,19 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
     """displays cities by a state on an HTML template"""
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown(self):
     """remove the current SQLAlchemy Session"""
     storage.close()
 
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
-
